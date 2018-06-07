48ca18f2280b52ba725da3f18b0529acb58975b6
Oksana Tkachenko <oksana@crystax.net>
2018-06-07 10:48:57 +0300

    changed width for mobile menu

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
   $('.catalog-filter-header').on('click', function(e) {
     e.preventDefault();
     $('.catalog-filter-list').toggleClass('mobile-open');
-/*    $('.catalog-filter-button').hide('.catalog-filter-list');*/
-
   });
 });
 
