53f8a04ee1d1267af3d1a770bb826746b7ed1762
Oksana Tkachenko <oksana@crystax.net>
2018-06-08 09:36:52 +0300

    added class selected

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ $(document).ready(function() {
     $('.catalog-filter-list').toggleClass('show');
   });
 
-  $('.checkbox').bind('click', function(e) {
+  $('.checkbox').on('click', function(e) {
     $(this).addClass('selected');
   });
 
-  $('.catalog-filter-form').submit(function(e) {
-    console.log('send');
-    return;
+  $('.btn-applying').click(function() {
+    $(this).parents('.catalog-filter-item').toggleClass('selected');
   });
-
 });
 
